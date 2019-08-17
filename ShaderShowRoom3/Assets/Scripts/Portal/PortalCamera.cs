d5fda51ad022547c7a2fe51594d4de2ebdad2b5d
Forve <tobias.forve@gmx.de>
2019-08-17 19:06:21 +0200

    Portal Start

---
+++ ShaderShowRoom3/Assets/Scripts/Portal/PortalCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    public Transform playerCam;
+    public Transform portal;
+    public Transform otherPortal;
+
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = playerCam.position - otherPortal.position;
+        this.transform.position = portal.position + playerOffsetFromPortal;
+    }
+}
