945429499f90bf249272852f0d94e2700bfecedf
Forve <tobias.forve@gmx.de>
2019-08-17 14:04:29 +0200

    Added R2D2, Movement and Light

---
+++ ShaderShowRoom3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 0.7f;
+    public float runSpeed = 1.5f;
+
+    public float turnSmoothTime = 0.2f;
+    private float turnSmoothVelocity;
+
+    public float speedSmoothTime = 0.1f;
+    private float speedSmoothVelocity;
+    private float currentSpeed;
+
+    // private Animator animator;
+    private Transform cameraTrans;
+
+    void Start()
+    {
+        // animator = GetComponent<Animator>();'
+        cameraTrans = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraTrans.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+
+        bool running = Input.GetKey (KeyCode.LeftShift);
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        this.transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+        // not needed here without Animation. but for later
+        // float animationSpeedPerc = ((running) ? 1: 0.5f) * inputDir.magnitude;
+        // animator.SetFloat("speedPercent", animationSpeedPerc, speedSmoothTime, Time*deltaTime);
+    }
+}
---
+++ ShaderShowRoom3/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public bool lockCursor;
+    public float mouseSensitivity = 10.0f;
+    public Transform target;
+    public float distanceFromTarget = 2.0f;
+
+    public Vector2 pitchMinMax = new Vector2(-40.0f, 85.0f);        // to clamp Rotation in y
+
+    public float rotationSmoothTime = 0.12f;                        // smoothing out the Camera
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+
+
+    private float yaw;
+    private float pitch;
+
+    void Start()
+    {
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X");
+        pitch -= Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * distanceFromTarget;
+    }
+}
